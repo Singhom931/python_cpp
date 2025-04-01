3f40ddacb68a5914d7d33a572acdec69b671e10d
Singhom931 <singhom931@gmail.com>
2025-04-01 21:53:23 +0530

    v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python_to_cpp", 
-    version="0.1.0a1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[],
     entry_points={
