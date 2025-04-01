32bd833b40cf6c53771401be288ab4b2825fc724
Singhom931 <singhom931@gmail.com>
2025-04-01 22:04:02 +0530

    define dir in setup

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="python_to_cpp", 
-    version="0.1.2",
-    packages=find_packages(),
+    version="0.1.3",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"}, 
     install_requires=[],
     entry_points={
         "console_scripts": [
